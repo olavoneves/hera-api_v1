99e77d91ec469cddee5d1058a7afc2500177ff24
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-10-15 20:51:49 -0300

    Adiciona DAO de medico

--- src/main/java/br/com/fiap/bo/MedicoBO.java
+++ src/main/java/br/com/fiap/bo/MedicoBO.java
@@ -1,4 +1,57 @@
 package br.com.fiap.bo;
 
+import br.com.fiap.dao.MedicoDAO;
+import br.com.fiap.dao.UsuarioDAO;
+import br.com.fiap.to.MedicoTO;
+import br.com.fiap.to.UsuarioTO;
+
+import java.util.ArrayList;
+
 public class MedicoBO {
+    private MedicoDAO medicoDAO;
+
+    public MedicoTO save(MedicoTO medico) {
+        usuarioDAO = new UsuarioDAO();
+
+        return usuarioDAO.save(usuario);
+    }
+
+    public MedicoTO update(MedicoTO medico) {
+        usuarioDAO = new UsuarioDAO();
+
+        // Se id for vazio
+        if (usuario.getId() == null) {
+            return null;
+        }
+
+        return usuarioDAO.update(usuario);
+    }
+
+    public boolean delete(Long id) {
+        usuarioDAO = new UsuarioDAO();
+
+        // Se id for vazio
+        if (id == null) {
+            return false;
+        }
+
+        return usuarioDAO.delete(id);
+    }
+
+    public MedicoTO findById(Long id) {
+        usuarioDAO = new UsuarioDAO();
+
+        // Se id for vazio
+        if (id == null) {
+            return null;
+        }
+
+        return usuarioDAO.findById(id);
+    }
+
+    public ArrayList<MedicoTO> findAll() {
+        usuarioDAO = new UsuarioDAO();
+
+        return usuarioDAO.findAll();
+    }
 }
--- src/main/java/br/com/fiap/dao/MedicoDAO.java
+++ src/main/java/br/com/fiap/dao/MedicoDAO.java
@@ -1,5 +1,132 @@
 package br.com.fiap.dao;
 
+import br.com.fiap.to.MedicoTO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class MedicoDAO {
 
+    public MedicoTO save(MedicoTO medico) {
+        String sql = "INSERT INTO T_HR_MEDICOS(nome, crm, especialidade, email, telefone, status) VALUES(?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, medico.getNome());
+            preparedStatement.setString(2, medico.getCrm());
+            preparedStatement.setString(3, medico.getEspecialidade());
+            preparedStatement.setString(4, medico.getEmail());
+            preparedStatement.setLong(5, medico.getTelefone().getId());
+            preparedStatement.setString(6, medico.getStatus());
+            if (preparedStatement.executeUpdate() > 0) {
+                return medico;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao criar medico: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return null;
+    }
+
+    public MedicoTO update(MedicoTO medico) {
+        String sql = "UPDATE T_HR_MEDICOS SET nome = ?, crm = ?, especialidade = ?, email = ?, telefone = ?, status = ? WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, medico.getNome());
+            preparedStatement.setString(2, medico.getCrm());
+            preparedStatement.setString(3, medico.getEspecialidade());
+            preparedStatement.setString(4, medico.getEmail());
+            preparedStatement.setLong(5, medico.getTelefone().getId());
+            preparedStatement.setString(6, medico.getStatus());
+            preparedStatement.setLong(7, medico.getId());
+            if (preparedStatement.executeUpdate() > 0) {
+                return medico;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar medico: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return null;
+    }
+
+    public boolean delete(Long id) {
+        String sql = "DELETE FROM T_HR_MEDICOS WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao excluir medico: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return false;
+    }
+
+    public MedicoTO findById(Long id) {
+        String sql = "SELECT * FROM T_HR_MEDICOS WHERE id = ?";
+        MedicoTO medico = null;
+
+        try (PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                medico = new MedicoTO();
+                medico.setId(resultSet.getLong("id"));
+                medico.setNome(resultSet.getString("nome"));
+                medico.setCrm(resultSet.getString("crm"));
+                medico.setEspecialidade(resultSet.getString("especialidade"));
+                medico.setEmail(resultSet.getString("email"));
+                medico.setTelefone(resultSet.getObject());
+                medico.setStatus(resultSet.getString("status"));
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar medico: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return medico;
+    }
+
+    public ArrayList<MedicoTO> findAll() {
+        String sql = "SELECT * FROM T_HR_MEDICOS ORDER BY ID";
+        ArrayList<MedicoTO> medicos = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet != null) {
+                while (resultSet.next()) {
+                    MedicoTO medico = new MedicoTO();
+                    medico.setId(resultSet.getLong("id"));
+                    medico.setNome(resultSet.getString("nome"));
+                    medico.setCrm(resultSet.getString("crm"));
+                    medico.setEspecialidade(resultSet.getString("especialidade"));
+                    medico.setEmail(resultSet.getString("email"));
+                    medico.setTelefone(resultSet.getObject());
+                    medico.setStatus(resultSet.getString("status"));
+                    medicos.add(medico);
+                }
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar medicos: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return medicos;
+    }
 }
