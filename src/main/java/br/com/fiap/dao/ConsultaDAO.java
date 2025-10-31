8f9dffbe63d4d8f49114053733d463a4169f88a7
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-10-30 22:13:43 -0300

    Modifica findAll de ConsultaDAO

--- src/main/java/br/com/fiap/dao/ConsultaDAO.java
+++ src/main/java/br/com/fiap/dao/ConsultaDAO.java
@@ -3,6 +3,7 @@ package br.com.fiap.dao;
 import br.com.fiap.to.ConsultaTO;
 import br.com.fiap.to.MedicoTO;
 import br.com.fiap.to.PacienteTO;
+import br.com.fiap.to.TelefoneTO;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -126,28 +127,56 @@ public class ConsultaDAO {
     }
 
     public ArrayList<ConsultaTO> findAll() {
-        String sql = "SELECT * FROM T_HR_CONSULTAS ORDER BY id_consulta";
+        String sql = """
+        SELECT 
+            c.id_consulta,
+            c.dt_consulta,
+            c.hr_consulta,
+            c.st_consulta,
+            c.tp_consulta,
+            c.ds_observacoes,
+            c.lk_teleconsulta,
+            
+            p.id_paciente,
+            p.nm_paciente,
+            p.em_paciente,
+            p.sg_sexo,
+            p.st_paciente,
+            
+            m.id_medico,
+            m.nm_medico,
+            m.cd_crm,
+            m.ds_especialidade,
+            m.em_medico,
+            m.st_medico,
+            
+            t1.id_telefone AS id_tel_paciente,
+            t1.nr_ddd AS ddd_paciente,
+            t1.nr_telefone AS num_paciente,
+            t1.tp_telefone AS tipo_paciente,
+            
+            t2.id_telefone AS id_tel_medico,
+            t2.nr_ddd AS ddd_medico,
+            t2.nr_telefone AS num_medico,
+            t2.tp_telefone AS tipo_medico
+            
+        FROM T_HR_CONSULTAS c
+        INNER JOIN T_HR_PACIENTES p ON c.id_paciente = p.id_paciente
+        INNER JOIN T_HR_MEDICOS m ON c.id_medico = m.id_medico
+        LEFT JOIN T_HR_TELEFONES t1 ON p.id_telefone = t1.id_telefone
+        LEFT JOIN T_HR_TELEFONES t2 ON m.id_telefone = t2.id_telefone
+        ORDER BY c.id_consulta
+        """;
+
         ArrayList<ConsultaTO> consultas = new ArrayList<>();
 
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            conn = ConnectionFactory.getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 ConsultaTO consulta = new ConsultaTO();
                 consulta.setId(rs.getLong("id_consulta"));
-
-                PacienteDAO pacienteDAO = new PacienteDAO();
-                consulta.setPaciente(pacienteDAO.findById(rs.getLong("id_paciente")));
-
-                MedicoDAO medicoDAO = new MedicoDAO();
-                consulta.setMedico(medicoDAO.findById(rs.getLong("id_medico")));
-
                 consulta.setDataConsulta(rs.getDate("dt_consulta").toLocalDate());
                 consulta.setHorarioConsulta(rs.getTime("hr_consulta").toLocalTime());
                 consulta.setStatus(rs.getString("st_consulta"));
@@ -155,19 +184,43 @@ public class ConsultaDAO {
                 consulta.setObservacoes(rs.getString("ds_observacoes"));
                 consulta.setLinkTeleconsulta(rs.getString("lk_teleconsulta"));
 
+                PacienteTO paciente = new PacienteTO();
+                paciente.setId(rs.getLong("id_paciente"));
+                paciente.setNome(rs.getString("nm_paciente"));
+                paciente.setEmail(rs.getString("em_paciente"));
+                paciente.setSexo(rs.getString("sg_sexo"));
+                paciente.setStatus(rs.getString("st_paciente"));
+
+                TelefoneTO telPac = new TelefoneTO();
+                telPac.setId(rs.getLong("id_tel_paciente"));
+                telPac.setDdd(rs.getString("ddd_paciente"));
+                telPac.setNumero(rs.getString("num_paciente"));
+                telPac.setTipoDeTelefone(rs.getString("tipo_paciente"));
+                paciente.setTelefone(telPac);
+
+                MedicoTO medico = new MedicoTO();
+                medico.setId(rs.getLong("id_medico"));
+                medico.setNome(rs.getString("nm_medico"));
+                medico.setCrm(rs.getString("cd_crm"));
+                medico.setEspecialidade(rs.getString("ds_especialidade"));
+                medico.setEmail(rs.getString("em_medico"));
+                medico.setStatus(rs.getString("st_medico"));
+
+                TelefoneTO telMed = new TelefoneTO();
+                telMed.setId(rs.getLong("id_tel_medico"));
+                telMed.setDdd(rs.getString("ddd_medico"));
+                telMed.setNumero(rs.getString("num_medico"));
+                telMed.setTipoDeTelefone(rs.getString("tipo_medico"));
+                medico.setTelefone(telMed);
+
+                consulta.setPaciente(paciente);
+                consulta.setMedico(medico);
+
                 consultas.add(consulta);
             }
 
         } catch (Exception e) {
             System.out.println("Erro ao buscar consultas: " + e.getMessage());
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (ps != null) ps.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
 
         return consultas;
